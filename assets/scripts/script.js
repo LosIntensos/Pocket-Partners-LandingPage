51ff4bdadabab30fd5735893cd1977b3aa45f88f
Fiorella <U202120418@upc.edu.pe>
2024-08-29 14:36:54 -0500

    feat(landingpage): dark mode implemented

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -89,3 +89,17 @@ const allCard = Array.from(document.querySelectorAll('.card'));
 				}
 			})
 		})
+
+
+//Dark Mode
+
+var icon = document.getElementById('icon');
+
+icon.onclick = function() {	
+	document.body.classList.toggle('dark-theme');
+	if(document.body.classList.contains('dark-theme')){
+		icon.src = 'assets/img/sun.png';
+	} else {
+		icon.src = 'assets/img/moon.png';
+	}
+}
